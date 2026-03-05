761d7c376171cf0dd6579b8ec2b74e33d0eb5d0e
Charles Fiedler <charlesfiedleriii@gmail.com>
2026-03-05 11:12:29 -0800

    initial Swift project commit

---
+++ AidMeHD/AidMeHDApp.swift
@@ -0,0 +1,17 @@
+//
+//  AidMeHDApp.swift
+//  AidMeHD
+//
+//  Created by Charles Fiedler on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct AidMeHDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
