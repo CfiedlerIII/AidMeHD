ee8df9ac408e70a47ec16e315f80c58e066ba246
Charles Fiedler <charlesfiedleriii@gmail.com>
2026-03-09 19:40:10 -0700

    feat: added sign-in with Apple capability feat: added Firebase and Sign in with Google setup and configuration feat: added Google and Apple sign-in managers feat: added an AuthManager to handle single-sign-on requests feat: added basic login and home views feat: updated the RootView to handle view updates based on account login status

--- AidMeHD/AidMeHDApp.swift
+++ AidMeHD/AidMeHDApp.swift
@@ -5,13 +5,32 @@
 //  Created by Charles Fiedler on 3/5/26.
 //
 
+import FirebaseCore
+import GoogleSignIn
 import SwiftUI
 
 @main
 struct AidMeHDApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+  @StateObject var authManager: AuthManager
+
+  init() {
+    FirebaseApp.configure()
+    if let clientID = FirebaseApp.app()?.options.clientID {
+      let config = GIDConfiguration(clientID: clientID)
+      GIDSignIn.sharedInstance.configuration = config
+    }
+    let authManager = AuthManager()
+    _authManager = StateObject(wrappedValue: authManager)
+  }
+
+  var body: some Scene {
+    WindowGroup {
+      RootView()
+        .environmentObject(authManager)
+        .onOpenURL { url in
+          //Handle Google Oauth URL
+          GIDSignIn.sharedInstance.handle(url)
         }
     }
+  }
 }
---
+++ AidMeHD/HomeView.swift
@@ -0,0 +1,122 @@
+//
+//  HomeView.swift
+//  AuthLogin
+//
+//  Created by Marwa Abou Niaaj on 29/11/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+  @EnvironmentObject var authManager: AuthManager
+  @State private var showLoginSheet = false
+  @State private var showDeleteAccountAlert = false
+
+  var body: some View {
+    NavigationStack {
+      VStack(spacing: 16) {
+        VStack(alignment: .leading) {
+          if authManager.authState == .signedIn {
+            Text(authManager.user?.displayName ?? "Name placeholder")
+              .font(.headline)
+
+            Text(authManager.user?.email ?? "Email placeholder")
+              .font(.subheadline)
+          }
+          else {
+            Text("Sign-in to view data!")
+              .font(.headline)
+          }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(12)
+        .padding()
+
+        Spacer()
+
+        HStack {
+          // Show `Sign out` iff user is not anonymous,
+          // otherwise show `Sign-in` to present LoginView() when tapped.
+          Button {
+            if authManager.authState != .signedIn {
+              showLoginSheet = true
+            } else {
+              signOut()
+            }
+          } label: {
+            Text(authManager.authState != .signedIn ? "Sign-in" :"Sign out")
+              .font(.body.bold())
+              .frame(width: 150, height: 45, alignment: .center)
+              .foregroundStyle(.mint)
+              .background(.blue)
+              .cornerRadius(10)
+          }
+
+          // Delete Account
+          Button {
+            showDeleteAccountAlert = true
+          } label: {
+            Text("Delete Account")
+              .font(.body.bold())
+              .frame(width: 150, height: 45, alignment: .center)
+              .foregroundStyle(.red)
+              .background(.blue)
+              .cornerRadius(10)
+          }
+        }
+      }
+      .padding()
+      .frame(maxWidth: .infinity, maxHeight: .infinity)
+      .background(.mint)
+      .navigationTitle("Welcome")
+
+      .sheet(isPresented: $showLoginSheet) {
+        LoginView()
+      }
+      .confirmationDialog("Delete Account", isPresented: $showDeleteAccountAlert) {
+        Button("Yes, Delete", role: .destructive) {
+          Task {
+            do {
+              try await authManager.deleteUserAccount()
+            }
+            catch AuthErrors.ReauthenticateApple {
+              // AppleID re-authentication failed
+            }
+            catch AuthErrors.RevokeAppleID {
+              // AppleID token revocation failed
+            }
+            catch AuthErrors.ReauthenticateGoogle {
+              // Google re-authentication failed
+            }
+            catch AuthErrors.RevokeGoogle {
+              // Google token revocation failed
+            }
+            catch {
+              // Show generic error message
+            }
+          }
+        }
+      } message: {
+        Text("Deleting account is permanent. Are you sure you want to delete your account?")
+      }
+    }
+  }
+
+  func signOut() {
+    Task {
+      do {
+        try await authManager.signOut()
+      }
+      catch {
+        print("Error: \(error)")
+      }
+    }
+  }
+}
+
+#Preview {
+  HomeView()
+    .environmentObject(AuthManager())
+}
